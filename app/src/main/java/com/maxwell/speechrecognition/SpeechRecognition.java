549edb071f46fd7625bfe4fc9710b9086fd92801
Maxwell Obi <obi.maxwell@rocketmail.com>
2018-01-14 23:34:43 +0100

    initialize SpeechRecognitionPermissions, add Fragment to parent Activity and set permission request listener

--- app/src/main/java/com/maxwell/speechrecognition/SpeechRecognition.java
+++ app/src/main/java/com/maxwell/speechrecognition/SpeechRecognition.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 /**
  * Created by Maxwell on 13-Jan-18.
@@ -21,32 +22,36 @@ public class SpeechRecognition {
     private Context context;
     private SpeechRecognizer speechRecognizer;
     private Intent recognizerIntent;
-    private OnSpeechRecognitionListener onSpeechRecognitionListener;
-    private boolean enableOnlyOfflineRecognition = false;
-    private boolean requestPermission = true;
 
-    /**
-     * SpeechRecognition Constructor
-     */
-    public SpeechRecognition(){}
+    private OnSpeechRecognitionListener onSpeechRecognitionListener;
+    private SpeechRecognitionPermissions speechRecognitionPermissions;
+
+    private boolean enableOnlyOfflineRecognition = false;
+    private boolean handlePermissions = true;
 
     /**
      * Initialize the SpeechRecognition class with the
-     * application context instance requesting speech recognition
+     * application context instance requesting SpeechRecognition.
+     * It is recommended you initialize this class in your Constructor
+     * or in <strong>onCreate</strong> of your Activity.
+     *
      * @param  context  the application context (cannot be null)
      * @see    Context
      */
     public SpeechRecognition(Context context){
         this.context = context;
+        initialize();
     }
 
     /**
-     * Sets the application context instance requesting speech recognition
+     * Sets the application context instance requesting SpeechRecognition
+     *
      * @param  context  the application context (cannot be null)
      * @see    Context
      */
     public void setContext(@NonNull Context context){
         this.context = context;
+        initialize();
     }
 
     /**
@@ -63,14 +68,37 @@ public class SpeechRecognition {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * By default, {@link SpeechRecognition} requests for Audio permissions and returns the result
+     * through {@link OnSpeechRecognitionPermissionListener} which is implemented in your Activity/Context.
+     * You can decide to handle Audio permissions yourself by setting handleAudioPermissions to false.
+     *
+     * @param  handlePermissions  true or false whether to handle audio permissions yourself or not.
+     * @see    OnSpeechRecognitionPermissionListener
+     */
+    public void handleAudioPermissions(boolean handlePermissions){
+        this.handlePermissions = handlePermissions;
+    }
+
     public void startSpeechRecognition(){
         //TODO: check if speech recognition is enabled on this phone
+
+        //Permission is not given yet, and handlePermission is false, throw error
+        if(!speechRecognitionPermissions.isPermissionGiven(context)){
+            if(!handlePermissions)
+                throw new SecurityException(context.getString(R.string.security_exception_text));
+
+            speechRecognitionPermissions.requestPermissions();
+        }
+
     }
 
     /**
-     * Sets SpeechRecognition to use only offline recognition engine
-     * Disabled by default - meaning either internet or offline recognition engines may be used
-     * @param  onlyOfflineRecognition  true or false (false by default)
+     * Sets {@link SpeechRecognition} to use only it's offline recognition engine
+     * This is disabled by default - meaning that either internet or offline recognition
+     * engines may be used at anytime. <strong>It is highly recommended that this is set to false</strong>
+     *
+     * @param  onlyOfflineRecognition  true or false whether to use only offline recognition or not (false by default)
      * @since API level 23
      */
     public void useOnlyOfflineRecognition(boolean onlyOfflineRecognition){
@@ -79,8 +107,21 @@ public class SpeechRecognition {
 
     private void initialize(){
 
+         /*
+          * Initialize the SpeechRecognitionPermissions and set listener with
+          * OnSpeechRecognitionPermissionListener implemented by client
+         */
+        speechRecognitionPermissions = new SpeechRecognitionPermissions();
+        ((Activity) context).getFragmentManager()
+                .beginTransaction()
+                .add(speechRecognitionPermissions, SpeechRecognition.class.getSimpleName())
+                .commit();
+
+        OnSpeechRecognitionPermissionListener permissionListener = (OnSpeechRecognitionPermissionListener)context;
+        speechRecognitionPermissions.setSpeechRecognitionPermissionListener(permissionListener);
+
         /*
-         *Initialize the SpeechRecognizer and set listener with user implement onSpeechRecognizerListener
+         *Initialize the SpeechRecognizer and set listener with onSpeechRecognizerListener implemented by client
          */
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
         speechRecognizer.setRecognitionListener(new SpeechRecognitionListener(onSpeechRecognitionListener));
